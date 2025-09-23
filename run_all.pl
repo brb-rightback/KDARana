0e3da53c5ae636805c5c267e368d32aea0b12873
Benjamin Bogart <bbogart@uboonegpvm02.fnal.gov>
2025-09-23 16:42:38 -0500

    Change run all ordering

--- run_all.pl
+++ run_all.pl
@@ -2,8 +2,7 @@
 
 system("perl setup_config.pl $ARGV[0] $ARGV[1]");
 system("perl ./convert_histo.pl 1");
-system("./bin/merge_hist_xs -r0 -l0 -h1");
 system("perl run_det.pl");
 system("perl run_xf.pl");
 system("perl run_stat.pl");
-
+system("./bin/merge_hist_xs -r0 -l0 -h1");
