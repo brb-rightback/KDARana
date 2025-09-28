bb5c0bd12826ee2736685fc57a64edece62e5fcd
Benjamin Bogart <bbogart@uboonegpvm04.fnal.gov>
2025-09-28 16:11:23 -0500

    Update configs

--- run_stat.pl
+++ run_stat.pl
@@ -7,25 +7,21 @@ chomp($config);
 close $file;
 print "Running MC stat for $config\n";
 
-system("./bin/merge_hist -r0 -l0 -e2 > mcstat/mc_stat.log&");
-
-if($config =~ /"presel"/){
+if($config =~ /presel/ || $config eq "lowE" || $config eq "hiE" || $config eq "lowE_ns" || $config eq "hiE_ns" || $config =~ /antibdtsel/ || $config =~ /ktagged/){
   system("bin/stat_pred_cov_matrix -r0 -h1&");
   system("bin/stat_cov_matrix -r0 -h1");
-
 }else{
   open my $file, '<', "configurations/xs_ch.txt" or die "Cannot open configurations/xs_ch.txt: $!";
   my $sigch = <$file>;
   chomp($sigch);
   close $file;
   print "Signal channel is $sigch\n";
-  system("sed -i '/$sigch/s/7.*cv_spline.*0/7    cv_spline    1/' configurations/cov_input.txt");
+  system("sed -i '/$sigch/s/cv_spline.*0/cv_spline    1/' configurations/cov_input.txt");
   my $found = 0;
   open(my $fh, '<', "configurations/cov_input.txt");
   while (my $line = <$fh>) {
     my $pattern = $sigch . ".*7.*cv_spline.*1";
     if ($line =~ $pattern) {
-      system("perl run_mc_stat.pl");
       $found = 1;
       last;
     }
@@ -34,5 +30,8 @@ if($config =~ /"presel"/){
   if($found==0){
     print "WARNING editing cov_input.txt failed\n"
   }
-  system("sed -i '/$sigch/s/7.*cv_spline.*1/7    cv_spline    0/' configurations/cov_input.txt");
+  system("perl run_mc_stat.pl");
+  system("sed -i '/$sigch/s/cv_spline.*1/cv_spline    0/' configurations/cov_input.txt");
 }
+
+system("./bin/merge_hist -r0 -l0 -e2 > mcstat/mc_stat.log");
